df38877c8c1fbfd69a8c125e54d1eeb2c3b1a7a2
Yu Qin <sirius.qinyu@gmail.com>
2022-07-31 09:52:40 +0800

    change heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <div>
             <Toaster />
 
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-bold underline">This is admin page</h1>
             <CategoryNav />
             <BookList />
         </div>
